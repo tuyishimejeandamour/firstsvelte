6f7fb2522b5f3d8caa9c145e8d1f4b0e99f76474
damour <tuyishimejeand88@gmail.com>
2021-05-29 23:04:00 +0200

    adding reactive variable and statement

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,26 @@
 <script>
  let name ="dobby";
+ let lastname="";
+ let firstname="";
+
+ //reactive variable we use dollar sign and name of that reactive varialble
+
+ $: fullname =`${lastname} ${firstname}`;
+ //some time you may use also reactive statement and it will check every time either of the variable change
+ 
+ $: {
+	 //this is to mean that if any of the first and lastname change the this code block will run
+	 console.log(fullname);
+	 console.log(name);
+ }
+
+
 </script>
 
 <main>
-
-	<p>{name}</p>
-	<input type="text" bind:value={name}>
-
+	<p>{fullname}</p>
+	<input type="text" bind:value={firstname}>
+	<input type="text" bind:value={lastname}>
 </main>
 
 
